a7541b57da57a598c404e6f72ff7102eed7c86ed
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-02 12:25:05 +0530

    update stack

---
+++ stack/implementation of stack by linkedlist.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+    node(int data)
+    {
+        this->data=data;
+        next=NULL;
+    }
+};
+
+int main()
+{
+    
+}
