e090c3c5a985083ba6ecc682c341d66d03eae098
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-02 12:42:48 +0530

    stack using linked list

--- stack/implementation of stack by linkedlist.cpp
+++ stack/implementation of stack by linkedlist.cpp
@@ -11,8 +11,74 @@ struct node
         next=NULL;
     }
 };
+struct mystack
+{
+    node* head;
+    int size;
+    mystack()
+    {
+        head=NULL;
+        size=0;
+    }
+    void push(int data)
+    {
+        node* ptr=new node(data);
+        if(head==NULL)
+        {
+            head=ptr;
+            size++;
+            return ;
+        }
+        ptr->next=head;
+        head=ptr;
+        size++;
+        return ;
+        
+    }
+    void pop()
+    {
+        if(size==0)
+        {
+            cout<<"stack is empty"<<endl;
+            return ;
+        }
+        node* temp=head;
+        head=head->next;
+        delete(temp);
+        size--;
+        return ;
+    }
+    int top()
+    {
+        if(size==0)
+        {
+            cout<<"stack is empty"<<endl;
+            return INT_MAX;
+        }
+        return head->data;
+    }
+    bool empty()
+    {
+        return size==0;
+    }
+    int ssize()
+    {
+        return size;
+    }
+
+};
 
 int main()
 {
-    
+    mystack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout<<"the size of stack is "<<s.ssize()<<endl;
+    while(!s.empty())
+    {
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+    cout<<"the size of stack is "<<s.ssize()<<endl;
 }
