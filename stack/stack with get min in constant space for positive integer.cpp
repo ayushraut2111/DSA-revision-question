1f47dd951a5336838478c9f1e52181ae3e8f005b
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-04 15:53:34 +0530

    stack with get min in constant space for positive integer only

---
+++ stack/stack with get min in constant space for positive integer.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct mystack
+{
+    stack<int>s;
+    int min;
+    void pushh(int x)
+    {
+        if(s.empty())
+        {
+            s.push(x);
+            min=x;
+            return ;
+        }
+        if(x>=min)
+        {
+            s.push(x);
+            return ;
+        }
+        if(x<min)
+        {
+            s.push(x-min);
+            min=x;
+            return ;
+        }
+    }
+    int popp()
+    {
+        if(s.top()<0)
+        {
+            int temp=min;
+            min=temp+(-1)*s.top();
+            s.pop();
+            return temp;
+        }
+        int temp=s.top();
+        s.pop();
+        return temp;
+    }
+    int getmin()
+    {
+        return min;
+    }
+};
+int main()
+{
+    mystack s;
+    s.pushh(5);
+    s.pushh(10);
+    s.pushh(20);
+    cout<<s.getmin()<<endl;
+    s.pushh(2);
+    s.popp();
+    s.pushh(6);
+    s.pushh(4);
+    cout<<s.getmin()<<endl;
+
+}
