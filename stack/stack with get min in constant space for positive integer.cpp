b612b76228abf6bca6a7c3a913cb21bfb8d92642
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-04 16:04:47 +0530

    stack with get min in constant space for positive integer only

--- stack/stack with get min in constant space for positive integer.cpp
+++ stack/stack with get min in constant space for positive integer.cpp
@@ -57,3 +57,8 @@ int main()
     cout<<s.getmin()<<endl;
 
 }
+
+// the idea behind solving this question is when we get the minimum number then current number then instead of pushing that umber we push
+// min-x and then update the min as current number so while popping when we get negative number in stack it means this number is the 
+// minimum number so we update the minimum to prev minimum because we are now poopint the cureent minimum so we update the 
+// minimum as min- current popped element
