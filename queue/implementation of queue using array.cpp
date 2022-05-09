cf23a6c6fba2003a5df18fb9f67b71a4770e7b54
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-09 11:44:01 +0530

    queue using array

---
+++ queue/implementation of queue using array.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct myqueue{
+    int size,cap;
+    int *arr;
+    myqueue(int cap)
+    {
+        this->cap=cap;
+        size=0;
+        arr=new int[cap];
+    }
+    void push(int x)
+    {
+        if(size==cap)
+        {
+            cout<<"queue is full"<<endl;
+            return;
+        }
+        arr[size]=x;
+        size++;
+    }
+    int pop()  // in this implementation only pop opeartion is costlier rest other opeartions are of constant time
+    {
+        if(size==0)
+        {
+            cout<<"queue is empty"<<endl;
+            return -1;
+        }
+        if(size==1)
+        {
+            int x=arr[0];
+            size--;
+            return x;
+        }
+        int x=arr[0];
+        for(int i=0;i<size-1;i++)   // in this implementation for pop operation we are moving every elements one position back 
+        {
+            arr[i]=arr[i+1];
+        }
+        size--;
+        return x;
+    }
+    int qsize()
+    {
+        return size;
+    }
+    bool isempty()
+    {
+        return size==0;
+    }
+    int front()
+    {
+        return arr[0];
+    }
+
+};
+int main()
+{
+    myqueue q(4);
+    q.push(1);
+    cout<<q.front()<<endl;
+    q.push(2);
+    cout<<q.pop()<<endl;
+    cout<<q.front()<<endl;
+    q.pop();
+    q.isempty()?cout<<"empty"<<endl:cout<<"not empty"<<endl;
+}
