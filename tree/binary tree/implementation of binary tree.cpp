68dc37de3342334e9e22834eaadf63506ad2ce26
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-14 01:11:25 +0530

    insertion in binary tree

---
+++ tree/binary tree/implementation of binary tree.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct node{
+    int data;
+    node* left,*right;
+    node(int x)
+    {
+        data=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+node* insert(node* root,int x)
+{
+    node* ptr=new node(x);
+    if(root==NULL)
+    {
+        root=ptr;
+        return root;
+    }
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* temp=q.front();
+        q.pop();
+        if(temp->left==NULL)
+        {
+            temp->left=ptr;
+            break;
+        }
+        else
+        {
+            q.push(temp->left);
+        }
+        if(temp->right==NULL)
+        {
+            temp->right=ptr;
+            break;
+        }
+        else
+        {
+            q.push(temp->right);
+        }
+    }
+    return root;
+}
+void inordertraversal(node*root)
+{
+    if(root==NULL)
+    return;
+    inordertraversal(root->left);
+    cout<<root->data<<endl;
+    inordertraversal(root->right);
+}
+
+int main()
+{
+    node* root=NULL;
+    root=insert(root,1);
+    root=insert(root,2);
+    root=insert(root,3);
+    inordertraversal(root);
+
+}
