e01d01d43f62d0b7cebdfb797b3ad544f3ed772e
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-05-23 01:09:29 +0530

    lowest common ancestor naive solution

--- tree/binary tree/LCA of a binary tree.cpp
+++ tree/binary tree/LCA of a binary tree.cpp
@@ -46,10 +46,58 @@ node* insert(node* root,int x)
     }
     return root;
 }
+bool findpath(node* root,vector<node*>&v,int n)
+{
+    if(root==NULL)
+    return false;
+    v.push_back(root);
+    if(root->data==n)
+    return true;
+    bool x=findpath(root->left,v,n);
+    bool y=findpath(root->right,v,n);
+    if(x||y)
+    {
+        return true;
+    }
+    v.pop_back();
+    return false;
+}
+int commonancestor(node* root,int x,int y)
+{
+    if(root==NULL)
+    {
+        cout<<"tree is empty"<<endl;
+        return -1;
+
+    }
+    vector<node*>v1,v2;
+    findpath(root,v1,x);
+    findpath(root,v2,y);
+    reverse(v1.begin(),v1.end());
+    for(auto x:v1)
+    {
+        if(find(v2.begin(),v2.end(),x)!=v2.end())
+        {
+            return x->data;
+        }
+    }
+    return -1;
+
+}
 int main()
 {
     node* root=NULL;
     root=insert(root,1);
-    root=insert(root,2);
-    root=insert(root,3);
+    root=insert(root,5);
+    root=insert(root,6);
+    root->left->left=new node(7);
+    root->left->right=new node(2);
+    root->left->left->left=new node(4);
+    root->left->right->right=new node(8);
+    root->left->right->left=new node(9);
+    root->left->right->left->left=new node(3);
+    cout<<commonancestor(root,3,8)<<endl;
 }
+
+// the logic behind this is to first find the paths of both the nodes and then finding the last common node and that will be the lowest common ancestor as it is coming at last
+// so to find the last common just traverse the any one of the vector from last and return the first common node
--- tree/test.cpp
+++ tree/test.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 int main()
 {
-    cout<<"hello world";
+    vector<int>v(3);
+    v[0]=1;
+    v[1]=2;
+    v[2]=3;
+    if(find(v.begin(),v.end(),2)!=v.end())
+    {
+        cout<<"found at"<<find(v.begin(),v.end(),2)-v.begin();
+    }
+    else
+    cout<<"not found";
 }
