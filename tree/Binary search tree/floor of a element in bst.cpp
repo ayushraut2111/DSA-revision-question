17678457579655b0ef6d9dc6c85ee3e9fe89f035
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-08-18 17:33:48 +0530

    floor of a element

---
+++ tree/Binary search tree/floor of a element in bst.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct node{
+    int data;
+    node* left,*right;
+    node(int data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+node* insert(node* root,int data)
+{
+    node* ptr=new node(data);
+    if(root==NULL)
+    {
+        root=ptr;
+        return root;
+    }
+    if(data<root->data)
+    {
+        root->left=insert(root->left,data);
+    }
+    else
+    {
+        root->right=insert(root->right,data);
+    }
+    return root;
+}
+node* floor(node* root,int x)
+{
+    if(root==NULL)
+    {
+        cout<<"tree is empty"<<endl;
+        return root;
+    }
+    node* res=NULL,*temp=root;
+    while(temp!=NULL)
+    {
+        if(temp->data==x)
+        return temp;
+        else if(temp->data>x)
+        temp=temp->left;
+        else
+        {
+            res=temp;
+            temp=temp->right;
+        }
+    }
+    return res;
+}
+int main()
+{
+    node* root=NULL;
+    root=insert(root,2);
+    root=insert(root,1);
+    root=insert(root,22);
+    root=insert(root,23);
+    cout<<floor(root,1)->data;
+
+}
