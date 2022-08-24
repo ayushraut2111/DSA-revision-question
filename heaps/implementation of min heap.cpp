a5beecfed089d99812e822639d259f796923ccec
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-08-24 23:26:36 +0530

    min heap

---
+++ heaps/implementation of min heap.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+struct minheap{
+    int *arr;
+    int size,cap;
+    minheap(int c)
+    {
+        cap=c;
+        size=0;
+        arr=new int[cap];
+    }
+    
+};
+int main()
+{
+    
+}
