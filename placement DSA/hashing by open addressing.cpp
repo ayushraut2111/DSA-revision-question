0a31965bb8756a88f630d699b559f90c0280c51c
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-28 16:12:12 +0530

    hashtable by open addressing

--- placement DSA/hashing by open addressing.cpp
+++ placement DSA/hashing by open addressing.cpp
@@ -10,11 +10,43 @@ struct Myhash{
         arr=new int[c];
         for(int i=0;i<c;i++)
         {
-            arr[i]=-1;
+            arr[i]=NULL;
+        }
+    }
+    void inserthash(int x)
+    {
+        if(size==cap)
+        {
+            cout<<"hashtable is full"<<endl;
+            return;
+        }
+        int key=x%cap;
+        if(arr[key]==NULL||arr[key]==-1)
+        {
+            arr[key]=x;
+            size++;
+            return ;
+        }
+        while(arr[key]!=NULL&&arr[key]!=-1)
+        {
+            key=(key+1)%cap;
+        }
+        arr[key]=x;
+        size++;
+    }
+    void printhash()
+    {
+        for(int i=0;i<size;i++)
+        {
+            cout<<arr[i]<<endl;
         }
     }
 };
 int main()
 {
-    
+    Myhash h(7);
+    h.inserthash(1);
+    h.inserthash(1);
+    h.inserthash(1);
+    h.printhash();
 }
