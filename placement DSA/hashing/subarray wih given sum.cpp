92a9853013eecd7df360dc9562d44844ca7e0e06
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-30 16:23:50 +0530

    prefix with given sum

---
+++ placement DSA/hashing/subarray wih given sum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(auto &x:a)cin>>x;
+    unordered_set<int>s;
+    int sum=0;
+    for(auto x:a)
+    {
+        sum+=x;
+        if(sum==k)
+        {
+            cout<<"yes";
+            return 0;
+        }
+        if(s.find(sum-k)!=s.end())
+        {
+            cout<<"yes";
+            return 0;
+        }
+        s.insert(sum);
+    }
+    cout<<"no";
+}
