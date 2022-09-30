9858a3fef30878b18e4d91ac7723a2a7fdda514f
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-09-30 16:07:31 +0530

    pair with given sum

--- placement DSA/hashing/pair with given sum.cpp
+++ placement DSA/hashing/pair with given sum.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 int main()
 {
-    cin>>n;
-    vector<int>a(n),b(n);
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(auto &x:a)cin>>x;
+    unordered_set<int>s;
+    for(auto x:a)
+    {
+        if(s.find(k-x)==s.end())
+        {
+            s.insert(x);
+        }
+        else
+        {
+            cout<<"yes"<<endl;
+            return 0;
+        }
+    }
+    cout<<"no";
 }
