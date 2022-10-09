64d5317ea65558d41955240e1f85046d396096e6
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-10-09 23:07:16 +0530

    insert at begin

--- placement DSA/linked list/circular singly linked list.py
+++ placement DSA/linked list/circular singly linked list.py
@@ -1,3 +1,6 @@
+from unicodedata import name
+
+
 class node:
     def __init__(self,x):
         self.data=x
@@ -6,5 +9,30 @@ class node:
 class llist:
     def __init__(self):
         self.head=None
-    
-    
+
+    def insertbegin(self,x):
+        ptr=node(x)
+        if self.head is None:
+            self.head=ptr
+            self.head.next=self.head
+            return
+        temp=self.head
+        while temp.next is not self.head:
+            temp=temp.next
+        ptr.next=self.head
+        self.head=ptr
+        temp.next=self.head
+
+    def printlist(self):
+        temp=self.head
+        while temp.next is not self.head:
+            print(temp.data)
+            temp=temp.next
+        print(temp.data)
+
+if __name__=="__main__":
+    head=llist()
+    head.insertbegin(1)
+    head.insertbegin(2)
+    head.insertbegin(3)
+    head.printlist()
