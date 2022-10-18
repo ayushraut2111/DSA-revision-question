a935fdcb2c422b1befcc89e38504d2bd26dba624
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-10-18 21:27:36 +0530

    sorted insert

--- placement DSA/linked list/sorted insert in a linked list.py
+++ placement DSA/linked list/sorted insert in a linked list.py
@@ -22,6 +22,26 @@ class llist:
         while temp is not None:
             print(temp.data)
             temp=temp.next
+    
+    def sortedinsert(self,x):
+        ptr=node(x)
+        if x<=self.head.data:
+            ptr.next=self.head
+            self.head=ptr
+            return
+        temp=self.head
+        p=None
+        while x>temp.data and (temp.next is not None):
+            p=temp
+            temp=temp.next
+        ptr.next=temp
+        p.next=ptr
+
 
 if __name__=="__main__":
-    
+    head=llist()
+    head.insertlast(10)
+    head.insertlast(20)
+    head.insertlast(30)
+    head.sortedinsert(22)
+    head.printnode()
