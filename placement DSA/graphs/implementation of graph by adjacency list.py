bff5c6a2286d151b47a1d09720d9d3361d67bd15
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-10-31 20:17:41 +0530

    bfs

--- placement DSA/graphs/implementation of graph by adjacency list.py
+++ placement DSA/graphs/implementation of graph by adjacency list.py
@@ -7,10 +7,47 @@ class graph:
         self.a[v].append(u)
 
     def prnt(self):
-        print(self.a)
+        for i in self.a:
+            for x in i:
+                print(x,end=" ")
+            print()
+    
+    def bfs_connected(self,s):                        # this bfs is applied when the graph is connected
+        visited=[False for i in range(len(self.a))]
+        q=[]
+        visited[s]=True
+        q.append(s)
+        while q:
+            r=q.pop(0)
+            print(r, end=" ")
+            for i in self.a[r]:
+                if visited[i]==False:
+                    q.append(i)
+                    visited[i]=True
+
+    def bfs(self,i,visited):    # this is a bfs for connected graph
+        q=[]
+        q.append(i)
+        visited[i]=True
+        while q:
+            r=q.pop(0)
+            print(r,end=" ")
+            for i in self.a[r]:
+                if visited[i]==False:
+                    q.append(i)
+                    visited[i]=True
+
+    def bfs_dis(self):     # this is a function which is used to pass the disconnected node of a graph
+        visited=[False for i in range(len(self.a))]
+        for i in range(len(self.a)):
+            if visited[i]== False:
+                self.bfs(i,visited)
+
 
 if __name__=="__main__":
     g1=graph(4)
     g1.add_vertex(0,1)
-    g1.add_vertex(2,3)
-    g1.prnt()
+    g1.add_vertex(0,2)
+    g1.add_vertex(1,2)
+    g1.add_vertex(1,3)
+    g1.bfs_dis()
--- placement DSA/graphs/test.py
+++ placement DSA/graphs/test.py
@@ -1,10 +1,14 @@
-def change(a):
-    for i in range(len(a)):
-        a[i]=i+1
+# def change(a):
+#     for i in range(len(a)):
+#         a[i]=i+1
 
-a=[]
-for i in range(5):
-    a.append(i)
-print(a,end='\n\n')
-change(a)
+# a=[]
+# for i in range(5):
+#     a.append(i)
+# print(a,end='\n\n')
+# change(a)
+# print(a)
+
+a=[1,2,3]
+n=a.pop(0)
 print(a)
