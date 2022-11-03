f003f663a629c5f7cb1767e97a0d2464e555cdc3
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-03 23:42:57 +0530

    effivient k slosest element

--- placement DSA/heaps/k closest number.cpp
+++ placement DSA/heaps/k closest number.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 int main()
 {
-    priority_queue<int,vector<int>,greater<int>>pq;
+    // priority_queue<int,vector<int>,greater<int>>pq;
+    // int n,x,k;
+    // cin>>n>>x>>k;
+    // int a[n];
+    // for(int i=0;i<n;i++)
+    // {
+    // cin>>a[i];
+    // }
+    // for(int i=0;i<n;i++)
+    // {
+    //     pq.push(abs(a[i]-x));
+    // }
+    // while(k--&&!pq.empty())
+    // {
+    //     cout<<pq.top()+x<<endl;
+    //     pq.pop();
+    // }
+    priority_queue<int>pq;
     int n,x,k;
     cin>>n>>x>>k;
     int a[n];
@@ -11,14 +28,18 @@ int main()
     {
     cin>>a[i];
     }
-    for(int i=0;i<n;i++)
+    for(int i=0;i<k;i++)
     {
-        pq.push(abs(a[i]-x));
+        pq.push(a[i]-x);
     }
-    while(k--&&!pq.empty())
+    for(int i=k;i<n;i++)
+    {
+        pq.push(a[i]-x);
+        pq.pop();
+    }
+    while(!pq.empty())
     {
         cout<<pq.top()+x<<endl;
         pq.pop();
     }
-
 }
