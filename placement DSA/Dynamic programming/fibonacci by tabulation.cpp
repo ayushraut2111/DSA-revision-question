3fc3ed2013c32b4b2fc8e32e9554f012d890634d
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-08 01:42:15 +0530

    fibonnaci by dp

---
+++ placement DSA/Dynamic programming/fibonacci by tabulation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    // vector<int>dp(n+1);  // this is taking space complexity also o(n)
+    // dp[0]=0;dp[1]=1;
+    // for(int i=2;i<=n;i++)dp[i]=dp[i-1]+dp[i-2];   
+    // cout<<dp[n]<<endl;
+
+    // we can optimise it as constant space complexity as it requirs only 2 prev datas
+    int prev=0,prev1=1;
+    for(int i=2;i<=n;i++)
+    {
+        int x=prev+prev1;
+        prev=prev1;
+        prev1=x;
+    }
+    cout<<prev1<<endl;
+
+}
